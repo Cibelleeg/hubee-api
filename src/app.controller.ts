1db8a31f075a112107c8feb05729e349b1d3f23e
Matheus Oliveira <mojesus@unifesp.br>
2026-09-02 18:02:28 -0300

    feat(GOL-28): adiciona husky com validacao de commit e lint no pre-push

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+const testeSemUso = 123;
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
